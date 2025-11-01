fe381c8c7f2422a34b7494ed822ae74682f119ad
rajaryan2007 <rajaryan149150@gmail.com>
2025-11-02 04:16:37 +0530

    add goggle signup

--- src/components/auth/login-button.tsx
+++ src/components/auth/login-button.tsx
@@ -1,10 +1,19 @@
 "use client";
 
+import { signIn } from "@/lib/auth-client";
 import { Button } from "../ui/button";
 
 function LoginButton() {
+   const handleLogin = async ()=>{
+    await signIn.social({
+      provider:"google",
+      callbackURL:"/",
+    })
+   }
+
+
   return (
-    <Button className="w-full bg-teal-500 hover:bg-teal-700 text-white py-6 text-base font-medium">
+    <Button onClick={handleLogin} className="w-full bg-teal-500 hover:bg-teal-700 text-white py-6 text-base font-medium">
       <span> Sign In With Google</span>
     </Button>
   );
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,40 @@
 import { betterAuth } from "better-auth";
+import {drizzleAdapter} from "better-auth/adapters/drizzle"
+import { db } from "./db";
+import {admin} from "better-auth/plugins"
+import { nextCookies } from "better-auth/next-js";
+
+
+
+
 
 const adminRole = "admin";
 const userRole = "user";
 
 export const auth = betterAuth({
-  database: drizzleAdapter(db),
+  database: drizzleAdapter(db,{
+    provider:'pg'
+  }),
+  socialProviders:{
+    google:{
+      clientId:process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret:process.env.GOOGLE_CLIENT_SECRET as string,
+      mapProfileToUser:(profile)=>{
+        return{
+          name:profile.name,
+          email:profile.email,
+          Image:profile.picture,
+          role:userRole
+        }
+      }
+
+    }
+  },
+  plugins:[
+    admin({
+      adminRole:[adminRole],
+      defaultRole:userRole,
+    }),
+    nextCookies(),
+  ]
 });
